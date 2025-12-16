c4454faaf86080f2be8d8357ed602c6e73571296
nishan <nishanpradhan500@gmail.com>
2025-12-16 19:49:29 +0545

    basic widget

--- new_project/lib/main.dart
+++ new_project/lib/main.dart
@@ -11,12 +11,72 @@ class Bit extends StatelessWidget {
   Widget build(BuildContext context) {
     //material app is a root app
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         //refernce web
         // header --> appbar
         appBar: AppBar(
           backgroundColor: Colors.red,
-          title: Text("BIT", style: TextStyle(color: Colors.white)),
+          leading: Icon(Icons.menu, color: Colors.white),
+          centerTitle: true,
+          title: Text(
+            'Discover',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-Ne7oVV6Lx9uAnmJDUZrrLcGy8yzo1sXdpQ&s",
+                ),
+              ),
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            spacing: 10,
+            children: [
+              Container(height: 100, width: 100, color: Colors.red),
+              // SizedBox(height: 10),
+              Container(height: 100, width: 100, color: Colors.green),
+              // SizedBox(height: 100),
+              Container(height: 100, width: 100, color: Colors.yellow),
+
+              Image.asset("assets/img.png", height: 200),
+
+              //for text
+              Text("fsdf"),
+              Icon(Icons.add),
+              ElevatedButton(onPressed: () {}, child: Text("login")),
+              OutlinedButton(onPressed: () {}, child: Text("Outline")),
+              TextButton(onPressed: () {}, child: Text("Text button")),
+              // Row(
+              //   spacing: 10,
+              //   children: [
+              //     Text('Popular'),
+              //     //for Spacing one is   ##sizebox and another is spacing provided in row widget
+              //     Text('Featured'),
+              //     // SizedBox(width: 10),
+              //     Text('Most Visited'),
+              //     Text('Europe'),
+              //     Text('Asia'),
+              //     Text('Popular'),
+              //   ],
+              // ),
+
+              //Container, Column /Row, Basic UI Widgets: Text, Image, Icon, SizedBox, Padding, Button (Elevated,Text, Outline)
+              // Column(
+              //   spacing: 10,
+              //   children: [
+
+              //   ],
+              // ),
+            ],
+          ),
         ),
       ),
     );
