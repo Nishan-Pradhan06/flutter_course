5353604e069a6977b130665551abeab3febff063
Nishan-Pradhan06 <nishanpradhan500@gmail.com>
2026-01-03 20:59:00 +0545

    design

--- travel_app/lib/screens/initial_screen.dart
+++ travel_app/lib/screens/initial_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:new_project/screens/home_screen.dart';
+import 'package:new_project/widgets/custom_button.dart';
 
 class InitialScreen extends StatelessWidget {
   const InitialScreen({super.key});
@@ -9,25 +10,58 @@ class InitialScreen extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          Container(height: 500, width: double.maxFinite, color: Colors.red),
-          ElevatedButton(
-            onPressed: () {
-              //pushReplacement example
-              //push  -->stack
-              //pop
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) {
-                    return HomePage();
+          Container(
+            height: 500,
+            width: double.maxFinite,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30),
+              ),
+              image: DecorationImage(
+                image: NetworkImage(
+                  'https://images.unsplash.com/photo-1500835556837-99ac94a94552?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dHJhdmVsfGVufDB8fDB8fHww',
+                ),
+                fit: BoxFit.cover,
+              ),
+              color: Colors.red,
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 10,
+              children: [
+                Text(
+                  'Winter \nVacation Trips',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 45),
+                ),
+
+                Text(
+                  'Enjoy your winter vacations with warmth and amazing sightseeing on the mountains. Enjoy the best experience with us!',
+                ),
+
+                CustomButton(
+                  text: 'Let\'s Go!',
+                  textStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                  backgroundColor: Color(0xFF6A62B7),
+                  suffix: Image.asset('assets/arrow.png', width: 30),
+                  padding: EdgeInsets.symmetric(horizontal: 30),
+                  onPressed: () {
+                    Navigator.of(
+                      context,
+                    ).push(MaterialPageRoute(builder: (context) => HomePage()));
                   },
                 ),
-              );
-            },
-            child: Text(
-              'data',
-              style: TextStyle(fontFamily: 'Merriweather'),
-            ), //replace with your widg`etgi
+              ],
+            ),
           ),
         ],
       ),
--- travel_app/lib/widgets/custom_button.dart
+++ travel_app/lib/widgets/custom_button.dart
@@ -2,20 +2,71 @@ import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
   final String text;
-  final void Function()? onPressed;
-  final TextStyle? style;
+  final VoidCallback? onPressed;
+
+  // Text & icons
+  final TextStyle? textStyle;
+  final Widget? prefix;
+  final Widget? suffix;
+
+  // Style
+  final Color? backgroundColor;
+  final Color? foregroundColor;
+  final double borderRadius;
+  final double elevation;
+  final EdgeInsetsGeometry padding;
+  final BorderSide? border;
+
+  // Size
+  final double? width;
+  final double? height;
+
   const CustomButton({
     super.key,
     required this.text,
     this.onPressed,
-    this.style,
+    this.textStyle,
+    this.prefix,
+    this.suffix,
+    this.backgroundColor,
+    this.foregroundColor,
+    this.borderRadius = 50,
+    this.elevation = 0,
+    this.padding = const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+    this.border,
+    this.width,
+    this.height,
   });
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      child: Text(text, style: style),
+    return SizedBox(
+      width: width,
+      height: height,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: backgroundColor,
+          foregroundColor: foregroundColor,
+          elevation: elevation,
+          padding: padding,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(borderRadius),
+            side: border ?? BorderSide.none,
+          ),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (prefix != null) ...[prefix!, const SizedBox(width: 8)],
+
+            Text(text, style: textStyle),
+
+            if (suffix != null) ...[const SizedBox(width: 8), suffix!],
+          ],
+        ),
+      ),
     );
   }
 }
