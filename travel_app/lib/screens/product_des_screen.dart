5bc0306c9f4e4e772b0c73890515d62fd597f632
Nishan-Pradhan06 <nishanpradhan500@gmail.com>
2026-01-08 15:15:09 +0545

    ui

--- travel_app/lib/screens/product_des_screen.dart
+++ travel_app/lib/screens/product_des_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:new_project/widgets/custom_button.dart';
 
 class ProductDesScreen extends StatelessWidget {
   final String title;
@@ -56,6 +57,16 @@ class ProductDesScreen extends StatelessWidget {
                     style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                   ),
                   Row(
+                    spacing: 4,
+                    children: [
+                      Icon(Icons.location_pin, size: 16),
+                      Text(
+                        "Honshu, Japan",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  Row(
                     children: [
                       for (int i = 1; i <= 5; i++)
                         Icon(
@@ -77,11 +88,120 @@ class ProductDesScreen extends StatelessWidget {
                       ),
                     ],
                   ),
+                  SizedBox(height: 30),
+                  Row(
+                    children: [
+                      // Minus button
+                      Container(
+                        height: 40,
+                        width: 30,
+                        decoration: BoxDecoration(
+                          color: Color(0xFF7C6FFF),
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: IconButton(
+                          onPressed: () {
+                            // Decrement logic
+                          },
+                          icon: Icon(
+                            Icons.remove,
+                            color: Colors.white,
+                            size: 24,
+                          ),
+                          padding: EdgeInsets.zero,
+                        ),
+                      ),
+
+                      SizedBox(width: 6),
+
+                      // Counter display
+                      Container(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 8,
+                        ),
+                        child: Text(
+                          '5',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(width: 6),
+
+                      // Plus button
+                      Container(
+                        height: 40,
+                        width: 30,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Color(0xFF7C6FFF),
+                        ),
+                        child: IconButton(
+                          onPressed: () {
+                            // Increment logic
+                          },
+                          icon: Icon(Icons.add, color: Colors.white, size: 24),
+                          padding: EdgeInsets.zero,
+                        ),
+                      ),
+                      SizedBox(width: 30),
+                      Icon(Icons.timer),
+                      SizedBox(width: 6),
+                      Text(
+                        '5 Days',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
                   Text(
                     "Description",
                     style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   ),
                   Text(desc, style: TextStyle(fontSize: 16)),
+                  SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            "\$400",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 35,
+                              color: Color(0xFF7C6FFF),
+                            ),
+                          ),
+                          Text(
+                            '/Package',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              color: Color(0xFF7C6FFF),
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      CustomButton(
+                        text: 'Book Now',
+                        onPressed: () {},
+                        backgroundColor: Color(0xFF7C6FFF),
+                        textStyle: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
